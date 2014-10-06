4e296d4bc1953c1ec55308d1aefa3cd0befe25d8
Peter Gassner <peter@naehrstoff.ch>
2014-10-06 12:55:27 +0200

    Marry new tooltip functionality with existing HTML tooltip

--- sszvis/component/tooltip2.js
+++ sszvis/component/tooltip2.js
@@ -26,26 +26,24 @@ namespace('sszvis.component.tooltip2', function(module) {
         }
       });
 
-      var tooltip = props.layer.selectAll('.tooltip')
-        .data(data)
+      var headerText = sszvis.component.modularText()
+        .bold(function(d){
+          return Math.round(d.x);
+        });
 
-      tooltip.enter()
-        .append('circle')
-        .attr('pointer-events', 'none')
-        .attr('class', 'tooltip');
-
-      var radius = 10;
-
-      tooltip
-        .attr('r', radius)
-        .attr('fill', '#f00')
-        .attr('cx', -radius)
-        .attr('cy', -radius)
-        .attr('transform', function(d) {
-          return 'translate(' + d.x + ',' + d.y + ')'
+      var tooltipMaker = tooltipRenderer()
+        .orientation('bottom')
+        .x(function(d) {
+          return d.x;
         })
+        .y(function(d) {
+          return d.y;
+        })
+        .header(headerText);
 
-      tooltip.exit().remove();
+      props.layer
+        .datum(data)
+        .call(tooltipMaker);
 
     }
 
@@ -63,5 +61,89 @@ namespace('sszvis.component.tooltip2', function(module) {
 
     return component;
   };
+
+
+
+  //
+  // Tooltip renderer
+  // TODO: improve
+  //
+  tooltipRenderer = function() {
+    return d3.component()
+      .prop('x')
+      .prop('y')
+      .prop('header').header('')
+      .prop('body').body('')
+      .prop('visible')
+      .prop('orientation')
+      .prop('tipsize').tipsize(10)
+      .prop('style', function(s) { return s || {} })
+      .render(function(data) {
+        var selection = d3.select(this);
+        var props = selection.props();
+
+        var tooltip = selection.selectAll('.sszvis-tooltip')
+          .data(data);
+
+        tooltip.exit().remove();
+
+        tooltip.style({
+          left: function(d) { return props.x(d) + 'px' },
+          top: function(d) { return props.y(d) + 'px' }
+        });
+
+        var enterTooltip = tooltip.enter()
+          .append('div')
+          .style('pointer-events', 'none')
+          .classed('sszvis-tooltip', true);
+
+        var enterBox = enterTooltip.append('div')
+          .classed('sszvis-tooltip-box', true)
+          .style(props.style);
+
+        enterBox.append('div')
+          .classed('sszvis-tooltip-header', true);
+
+        enterBox.append('div')
+          .classed('sszvis-tooltip-body', true);
+
+        var enterTipholder = enterTooltip.append('div')
+          .classed('sszvis-tooltip-tipholder', true)
+          .classed('tip-top', props.orientation === 'top')
+          .classed('tip-bot', props.orientation === 'bottom')
+          .classed('tip-left', props.orientation === 'left')
+          .classed('tip-right', props.orientation === 'right');
+
+        var enterTip = enterTipholder.append('div')
+          .classed('sszvis-tooltip-tip', true)
+          .classed('tip-top', props.orientation === 'top')
+          .classed('tip-bot', props.orientation === 'bottom')
+          .classed('tip-left', props.orientation === 'left')
+          .classed('tip-right', props.orientation === 'right');
+
+        tooltip.selectAll('.sszvis-tooltip-header')
+          .data(data)
+          .html(props.header);
+
+        tooltip.selectAll('.sszvis-tooltip-body')
+          .data(data)
+          .html(props.body);
+
+        selection.selectAll('.sszvis-tooltip')
+          .each(function(d) {
+            var d3this = d3.select(this),
+                width = this.offsetWidth,
+                height = this.offsetHeight,
+                x = props.x(d),
+                y = props.y(d);
+
+            d3this.style({
+              left: x - width / 2 + 'px',
+              top: y - height - props.tipsize + 'px'
+            });
+          });
+      });
+   };
+
 
 });
