2371ca5f9ae2cda6328ff248ccf12f6a4f95b670
Peter Gassner <peter@naehrstoff.ch>
2014-10-15 08:39:49 +0200

    Small ht-kreise cleanup

--- sszvis/fn.js
+++ sszvis/fn.js
@@ -12,6 +12,33 @@ namespace('sszvis.fn', function(module) {
   }
 
   module.exports = {
+    /**
+     * Wraps a function of any arity (including nullary) in a function that
+     * accepts exactly `n` parameters. Any extraneous parameters will not be
+     * passed to the supplied function.
+     *
+     * @param {number} n The desired arity of the new function.
+     * @param {Function} fn The function to wrap.
+     * @return {Function} A new function wrapping `fn`. The new function is
+     * guaranteed to be of arity `n`.
+     */
+    arity: function(n, fn) {
+      switch (n) {
+        case 0: return function() {return fn.call(this);};
+        case 1: return function(a0) {return fn.call(this, a0);};
+        case 2: return function(a0, a1) {return fn.call(this, a0, a1);};
+        case 3: return function(a0, a1, a2) {return fn.call(this, a0, a1, a2);};
+        case 4: return function(a0, a1, a2, a3) {return fn.call(this, a0, a1, a2, a3);};
+        case 5: return function(a0, a1, a2, a3, a4) {return fn.call(this, a0, a1, a2, a3, a4);};
+        case 6: return function(a0, a1, a2, a3, a4, a5) {return fn.call(this, a0, a1, a2, a3, a4, a5);};
+        case 7: return function(a0, a1, a2, a3, a4, a5, a6) {return fn.call(this, a0, a1, a2, a3, a4, a5, a6);};
+        case 8: return function(a0, a1, a2, a3, a4, a5, a6, a7) {return fn.call(this, a0, a1, a2, a3, a4, a5, a6, a7);};
+        case 9: return function(a0, a1, a2, a3, a4, a5, a6, a7, a8) {return fn.call(this, a0, a1, a2, a3, a4, a5, a6, a7, a8);};
+        case 10: return function(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9) {return fn.call(this, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9);};
+        default: return fn;
+      }
+    },
+
     compose: function() {
       var fns = arguments,
           start = arguments.length - 1;
