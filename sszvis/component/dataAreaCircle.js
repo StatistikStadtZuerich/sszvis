a04e94bdbb70770df79f13888f15b313b3c56539
Mark Hintz <mark@interactivethings.com>
2014-10-20 18:22:03 +0200

    Add captions to circular data areas.

--- sszvis/component/dataAreaCircle.js
+++ sszvis/component/dataAreaCircle.js
@@ -10,6 +10,9 @@ namespace('sszvis.component.dataAreaCircle', function(module) {
       .prop('x', d3.functor)
       .prop('y', d3.functor)
       .prop('r', d3.functor)
+      .prop('dx', d3.functor)
+      .prop('dy', d3.functor)
+      .prop('caption', d3.functor)
       .render(function(data) {
         var selection = d3.select(this);
         var props = selection.props();
@@ -29,6 +32,20 @@ namespace('sszvis.component.dataAreaCircle', function(module) {
           .attr('cy', props.y)
           .attr('r', props.r)
           .attr('fill', 'url(#data-area-pattern)');
+
+        var dataCaptions = selection.selectAll('.sszvis-data-area-circle-caption')
+          .data(data);
+
+        dataCaptions.enter()
+          .append('text')
+          .classed('sszvis-data-area-circle-caption', true);
+
+        dataCaptions
+          .attr('x', props.x)
+          .attr('y', props.y)
+          .attr('dx', props.dx)
+          .attr('dy', props.dy)
+          .text(props.caption);
       });
   };
 
