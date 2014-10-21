dd5f8533e0cd7de013132ce71facd0a0623d0f5c
Mark Hintz <mark@interactivethings.com>
2014-10-21 15:54:28 +0200

    Add padding and touch events to behaviors.

--- sszvis/behavior/click.js
+++ sszvis/behavior/click.js
@@ -3,19 +3,29 @@
  * @return {d3.component}
  */
 namespace('sszvis.behavior.click', function(module) {
+  'use strict';
 
   module.exports = function() {
-    var event = d3.dispatch('mousedown', 'mouseup', 'click', 'drag');
+    var event = d3.dispatch('down', 'up', 'click', 'drag');
 
     var clickComponent = d3.component()
       .prop('xScale')
       .prop('yScale')
+      .prop('padding', function(p) {
+        var defaults = { top: 0, left: 0, bottom: 0, right: 0 };
+        for (var prop in p) { defaults[prop] = p[prop]; }
+        return defaults;
+      }).padding({})
       .render(function() {
         var selection = d3.select(this);
         var props = selection.props();
 
         var xExtent = props.xScale.range().sort(d3.ascending);
         var yExtent = props.yScale.range().sort(d3.ascending);
+        xExtent[0] -= props.padding.left;
+        xExtent[1] += props.padding.right;
+        yExtent[0] -= props.padding.top;
+        yExtent[1] += props.padding.bottom;
 
         var layer = selection.selectAll('[data-sszvis-behavior-click]')
           .data([0]);
@@ -24,7 +34,36 @@ namespace('sszvis.behavior.click', function(module) {
           .append('rect')
           .attr('data-sszvis-behavior-click', '');
 
-        var isDragging = false;
+        // defined in this scope in order to have access to props
+        function bindDragEvents(targetEl) {
+          // used to bind the 'drag' behavior in the event handlers for both touchstart and mousedown
+          d3.select(document)
+            .on('touchmove.sszvis-click', function() {
+              var invXY = invertXY(sszvis.fn.first(d3.touches(targetEl)), props.xScale, props.yScale);
+              event.drag(invXY[0], invXY[1]);
+            })
+            .on('touchend.sszvis-click', function() {
+              stopDragging();
+
+              // calling preventDefault here prevents the browser from sending imitation mouse events
+              // this is good because actual mouse events are already handled, and because often these
+              // imitation events are on a 300ms delay, which can make the user experience strange after-effects.
+              d3.event.preventDefault();
+            })
+            .on('mousemove.sszvis-click', function() {
+              var invXY = invertXY(d3.mouse(targetEl), props.xScale, props.yScale);
+              event.drag(invXY[0], invXY[1]);
+            })
+            .on('mouseup.sszvis-click', function() {
+              stopDragging();
+
+              var invXY = invertXY(d3.mouse(targetEl), props.xScale, props.yScale);
+              event.up(invXY[0], invXY[1]);
+            })
+            .on('mouseleave.sszvis-click', function() {
+              stopDragging();
+            });
+        }
 
         layer
           .attr('x', xExtent[0])
@@ -32,28 +71,21 @@ namespace('sszvis.behavior.click', function(module) {
           .attr('width',  xExtent[1] - xExtent[0])
           .attr('height', yExtent[1] - yExtent[0])
           .attr('fill', 'transparent')
-          .on('mousedown', function() {
-            var invXY = invertXY(d3.mouse(this), props.xScale, props.yScale);
-            this.__isDragging = true;
-            event.mousedown(invXY[0], invXY[1]);
+          .on('touchstart', function() {
+            bindDragEvents(this);
+
+            var invXY = invertXY(sszvis.fn.first(d3.touches(this)), props.xScale, props.yScale);
+            event.down(invXY[0], invXY[1]);
           })
-          .on('mouseup', function() {
+          .on('mousedown', function() {
+            bindDragEvents(this);
+
             var invXY = invertXY(d3.mouse(this), props.xScale, props.yScale);
-            this.__isDragging = false;
-            event.mouseup(invXY[0], invXY[1]);
+            event.down(invXY[0], invXY[1]);
           })
           .on('click', function() {
             var invXY = invertXY(d3.mouse(this), props.xScale, props.yScale);
             event.click(invXY[0], invXY[1]);
-          })
-          .on('mousemove', function() {
-            if (this.__isDragging) {
-              var invXY = invertXY(d3.mouse(this), props.xScale, props.yScale);
-              event.drag(invXY[0], invXY[1]);
-            }
-          })
-          .on('mouseout', function() {
-            this.__isDragging = false;
           });
       });
 
@@ -64,6 +96,15 @@ namespace('sszvis.behavior.click', function(module) {
 
   function invertXY(xy, xScale, yScale) {
     return [xScale.invert(xy[0]), yScale.invert(xy[1])];
+  }
+
+  function stopDragging() {
+    d3.select(document)
+      .on('touchmove.sszvis-click', null)
+      .on('touchend.sszvis-click', null)
+      .on('mousemove.sszvis-click', null)
+      .on('mouseup.sszvis-click', null)
+      .on('mouseleave.sszvis-click', null);
   }
 
 });
--- sszvis/behavior/move.js
+++ sszvis/behavior/move.js
@@ -3,6 +3,7 @@
  * @return {d3.component}
  */
 namespace('sszvis.behavior.move', function(module) {
+  'use strict';
 
   module.exports = function() {
     var event = d3.dispatch('start', 'move', 'end');
@@ -10,12 +11,21 @@ namespace('sszvis.behavior.move', function(module) {
     var moveComponent = d3.component()
       .prop('xScale')
       .prop('yScale')
+      .prop('padding', function(p) {
+        var defaults = { top: 0, left: 0, bottom: 0, right: 0 };
+        for (var prop in p) { defaults[prop] = p[prop]; }
+        return defaults;
+      }).padding({})
       .render(function() {
         var selection = d3.select(this);
         var props = selection.props();
 
         var xExtent = props.xScale.range().sort(d3.ascending);
         var yExtent = props.yScale.range().sort(d3.ascending);
+        xExtent[0] -= props.padding.left;
+        xExtent[1] += props.padding.right;
+        yExtent[0] -= props.padding.top;
+        yExtent[1] += props.padding.bottom;
 
         var layer = selection.selectAll('[data-sszvis-behavior-move]')
           .data([0]);
@@ -35,6 +45,17 @@ namespace('sszvis.behavior.move', function(module) {
           .on('mousemove', function() {
             var xy = d3.mouse(this);
             event.move(props.xScale.invert(xy[0]), props.yScale.invert(xy[1]));
+          })
+          .on('touchdown', event.start)
+          .on('touchmove', function() {
+            var xy = sszvis.fn.first(d3.touches(this));
+            event.move(props.xScale.invert(xy[0]), props.yScale.invert(xy[1]));
+          })
+          .on('touchend', function() {
+            event.end.apply(this, arguments);
+
+            // calling preventDefault here prevents the browser from sending imitation mouse events
+            d3.event.preventDefault();
           });
       });
 
