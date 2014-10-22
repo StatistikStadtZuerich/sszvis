7b7148a8802cc9320dedba0677741c923156df79
Mark Hintz <mark@interactivethings.com>
2014-10-22 12:59:45 +0200

    Add caption to the reference line.

--- sszvis/component/dataAreaLine.js
+++ sszvis/component/dataAreaLine.js
@@ -16,12 +16,17 @@ namespace('sszvis.component.dataAreaLine', function(module) {
       .prop('xScale')
       .prop('yScale')
       .prop('xRange')
+      .prop('dx', d3.functor).dx(0)
+      .prop('dy', d3.functor).dy(0)
+      .prop('caption', d3.functor)
       .render(function(data) {
         var selection = d3.select(this);
         var props = selection.props();
 
-        var y1 = props.yScale(props.m * props.xScale.invert(props.xRange[0]) + props.b);
-        var y2 = props.yScale(props.m * props.xScale.invert(props.xRange[1]) + props.b);
+        var x1 = props.xRange[0];
+        var x2 = props.xRange[1];
+        var y1 = props.yScale(props.m * props.xScale.invert(x1) + props.b);
+        var y2 = props.yScale(props.m * props.xScale.invert(x2) + props.b);
 
         var line = selection.selectAll('.sszvis-reference-line')
           .data(data);
@@ -33,10 +38,40 @@ namespace('sszvis.component.dataAreaLine', function(module) {
         line.exit().remove();
 
         line
-          .attr('x1', props.xRange[0])
+          .attr('x1', x1)
           .attr('y1', y1)
-          .attr('x2', props.xRange[1])
+          .attr('x2', x2)
           .attr('y2', y2);
+
+        if (props.caption) {
+          var caption = selection.selectAll('.sszvis-reference-line--caption')
+            .data([1]);
+
+          caption.enter()
+            .append('text')
+            .classed('sszvis-reference-line--caption', true);
+
+          caption.exit().remove();
+
+          caption
+            .attr('transform', function() {
+              var vx = x2 - x1;
+              var vy = y2 - y1;
+              var angle = Math.atan2(vy, vx) * 180 / Math.PI;
+              var rotation;
+              if (angle > 0) {
+                // in top half
+                rotation = angle < 90 ? -angle : angle;
+              } else {
+                // in bottom semicircle
+                rotation = angle > -90 ? -angle : angle; // display angle math is weird
+              }
+              return 'translate(' + ((x1 + x2) / 2) + ',' + ((y1 + y2) / 2) + ') rotate(' + (angle) + ')';
+            })
+            .attr('dx', props.dx)
+            .attr('dy', props.dy)
+            .text(props.caption);
+        }
       });
   };
 
