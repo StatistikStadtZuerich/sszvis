591a4d4e0536eafd44d989d6f237437fc19a9f8b
Peter Gassner <peter@naehrstoff.ch>
2014-11-03 16:08:49 +0100

    Line charts: use x/y instead of xValue/yValue

--- sszvis/component/line.js
+++ sszvis/component/line.js
@@ -5,14 +5,14 @@
  * The input data should be an array of arrays, where each inner array
  * contains the data points necessary to render a line. The line is then
  * composed of x- and y- values extracted from these data objects
- * using the xValue and yValue accessor functions.
+ * using the x and y accessor functions.
  *
  * Each data object in a line's array is passed to the x- and y- accessors, along with
  * that data object's index in the array. For more information, see the documentation for
  * d3.svg.line.
  *
- * @property {function} yValue            An accessor function for getting the y-value of the line
- * @property {function} xValue            An accessor function for getting the x-value of the line
+ * @property {function} x                 An accessor function for getting the x-value of the line
+ * @property {function} y                 An accessor function for getting the y-value of the line
  * @property {string, function} [stroke]  Either a string specifying the stroke color of the line or lines,
  *                                        or a function which, when passed the entire array representing the line,
  *                                        returns a value for the stroke. If left undefined, the stroke is black.
@@ -23,12 +23,13 @@
  * @return {d3.component}
  */
 namespace('sszvis.component.line', function(module) {
+  'use strict';
 
   module.exports = function() {
 
     return d3.component()
-      .prop('xValue')
-      .prop('yValue')
+      .prop('x')
+      .prop('y')
       .prop('stroke')
       .prop('strokeWidth')
       .render(function(data) {
@@ -39,9 +40,9 @@ namespace('sszvis.component.line', function(module) {
         // Layouts
 
         var line = d3.svg.line()
-          .defined(sszvis.fn.compose(sszvis.fn.not(isNaN), props.yValue))
-          .x(props.xValue)
-          .y(props.yValue);
+          .defined(sszvis.fn.compose(sszvis.fn.not(isNaN), props.y))
+          .x(props.x)
+          .y(props.y);
 
 
         // Rendering
@@ -63,6 +64,6 @@ namespace('sszvis.component.line', function(module) {
           .attr('stroke', props.stroke)
           .attr('stroke-width', props.strokeWidth);
       });
-  }
+  };
 
 });
