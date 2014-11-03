29fe84847de2998bbeb841d433abf7f0770bb937
Peter Gassner <peter@naehrstoff.ch>
2014-11-03 14:06:57 +0100

    Number formatting

--- sszvis.js
+++ sszvis.js
@@ -1483,58 +1483,60 @@ namespace('sszvis.fn', function(module) {
  * @module sszvis/format
  */
 namespace('sszvis.format', function(module) {
+  'use strict';
 
-  module.exports = (function() {
-    return {
-      /**
-       * Default formatter for text
-       * @param  {Number} d
-       * @return {String}   Fully formatted text
-       */
-      text: function(d) {
-        return String(d);
-      },
+  var format = module.exports = {
+    /**
+     * Format a number as an age
+     * @param  {number} d
+     * @return {string}
+     */
+    age: function(d) {
+      return String(Math.round(d));
+    },
 
-      /**
-       * formatter for no label
-       * @param  {String} d datum
-       * @return {String} the empty string
-       */
-      none: function(d) {
-        return '';
-      },
+    /**
+     * Formatter for no label
+     * @param  {string} d datum
+     * @return {string} the empty string
+     */
+    none: function(d) {
+      return '';
+    },
 
-      /**
-       * Format numbers according to the sszvis style guide
-       * @param  {Number} d
-       * @return {String} Fully formatted number
-       */
-      number: function(d) {
-        if (d >= 1e4) {
-          return d3.format(',.2r')(d);
-        } else if (d === 0) {
-          return 0;
-        } else {
-          return d3.format('.2r')(d);
-        }
-      },
-
-      age: function(d) {
-        return String(Math.round(d));
-      },
-
-      percent: function(d) {
-        if (d >= 1) {
-          return '100%';
-        } else if (d <= 0) {
-          return '0%';
-        } else {
-          return d3.format('%')(d);
-        }
+    /**
+     * Format numbers according to the sszvis style guide.
+     * @param  {number} d
+     * @return {string} Fully formatted number
+     */
+    number: function(d) {
+      if (d >= 1e4) {
+        return d3.format(',')(d);
+      } else if (d === 0) {
+        return 0;
+      } else {
+        return d;
       }
-    }
-  }());
+    },
 
+    /**
+     * Format percentages
+     * @param  {number} d A fraction, usually between 0 and 1
+     * @return {string}
+     */
+    percent: function(d) {
+      return format.number(d * 100) + ' %';
+    },
+
+    /**
+     * Default formatter for text
+     * @param  {number} d
+     * @return {string} Fully formatted text
+     */
+    text: function(d) {
+      return String(d);
+    }
+  };
 });
 
 
