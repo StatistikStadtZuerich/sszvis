213bbc0768285339a89e14b1d40d023575158460
Mark Hintz <mark@interactivethings.com>
2014-11-05 11:55:53 +0100

    Edit the global namespace function to enable extending namespaces and to prevent overwriting existing names or sub-namespaces.

--- sszvis.js
+++ sszvis.js
@@ -284,6 +284,24 @@
 
 (function(global){
 
+  function ns_extend(nsname, obj) {
+    var source;
+    for (var i = 2, l = arguments.length; i < l; ++i) {
+      source = arguments[i];
+      for (var name in source) {
+        if (source.hasOwnProperty(name)) {
+          if (typeof obj[name] !== 'undefined') throwNSOverwriteError(nsname, name);
+          obj[name] = source[name];
+        }
+      }
+    }
+    return obj;
+  }
+
+  function throwNSOverwriteError(nsName, nsTarget) {
+    throw new Error('in namespace: ' + nsName + ' - attempting to overwrite an existing name: ' + nsTarget);
+  }
+
   global.namespace = function(path, body) {
     var segments = path.split('.');
     var ancestors = segments.slice(0, segments.length - 1);
@@ -293,10 +311,20 @@
       return root[part];
     }, global);
 
-    var module = {exports: {}};
+    var originalExports = {};
+    var module = { exports: originalExports };
     body(module);
 
-    return ns[target] = module.exports;
+    var moduleExports = module.exports;
+    if (moduleExports === originalExports) {
+      if (typeof ns[target] === 'undefined') ns[target] = {};
+      ns_extend(path, ns[target], moduleExports);
+    } else {
+      if (typeof ns[target] !== 'undefined') throwNSOverwriteError(ancestors.join('.'), target);
+      ns[target] = moduleExports;
+    }
+
+    return ns[target];
   }
 
 }(window));
