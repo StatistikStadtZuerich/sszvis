5b04885466b95c8663639c48985523c25bf0c954
Peter Gassner <peter@naehrstoff.ch>
2014-11-06 11:09:49 +0100

    Further refactoring of the color2 scale (WIP)

--- sszvis/color2.js
+++ sszvis/color2.js
@@ -5,103 +5,94 @@
  *
  * instead of nested objects, this module exposes an api of just functions
  *
- * basicBlue
- * basicDeepBlue
- * plainWhite
+ * qual
+ * seqBlu
+ * seqRed
+ * seqGrn
+ * seqBrn
+ * divBlu
+ * divBluAlt
+ * divGrn
+ * divGrnAlt
  *
- * qual3
- * qual6
- * qual9
- * qual12
- *
- * qualRedBlu
- * qualGrnBrn
- *
- * qualBlu
- * qualRed
- * qualGrn
- * qualBrn
- * qualBluGryRed
- * qualGrnGryBrn
- *
- * linearBlu
- * linearRed
- * linearGrn
- * linearBrn
- * linearBluWhtRed
- * linearBluGryRed
- * linearGrnWhtBrn
- * linearGrnGryBrn
- *
- * logBlu
- * logRed
- * logGrn
- * logBrn
- * logBluWhtRed
- * logBluGryRed
- * logGrnWhtBrn
- * logGrnGryBrn
- *
- * powBlu
- * powRed
- * powGrn
- * powBrn
- * powBluWhtRed
- * powBluGryRed
- * powGrnWhtBrn
- * powGrnGryBrn
+ * darker
  */
-namespace('sszvis.color', function(module) {
+namespace('sszvis.color2', function(module) {
+  'use strict';
 
-  /* Values
-  ----------------------------------------------- */
-  module.exports.basicBlue = function() { return d3.lab("#6392C5"); };
-  module.exports.basicDeepBlue = function() { return d3.lab("#3A75B2"); };
-  module.exports.plainWhite = function() { return d3.lab("#FFFFFF"); };
+  var DARKEN = 0.6;
 
-  /* Color Arrays - private
-  ----------------------------------------------- */
-  function convertLab(a) { return a.map(function(c) { return d3.lab(c); }); }
+  var QUAL_COLORS = {
+    qual:       [
+                  '#B8CFE6', '#5182B3', '#F2CEC2',
+                  '#E67D73', '#FAEBAF', '#E6CF73',
+                  '#CFE6B8', '#94BF69', '#B8E6D2',
+                  '#60BF97', '#E6B7C7', '#CC6788'
+                ]
+  };
 
-  var qualColors = convertLab(["#b8cfe6", "#5182b3", "#e6b7c7", "#cc6788", "#f2cec2", "#e67d73", "#faebaf", "#e6cf73", "#cfe6b8", "#94bf69", "#b8e6d2", "#60bf97"]);
+  var SEQ_COLORS = {
+    seqBlu:     [ '#DDE9FE', '#3B76B3', '#343F4D' ],
+    seqRed:     [ '#FEECEC', '#CC6171', '#4D353A' ],
+    seqGrn:     [ '#D2DFDE', '#4A807C', '#2C3C3F' ],
+    seqBrn:     [ '#E9DFD6', '#A67D5A', '#4C3735' ]
+  };
+
+  var DIV_COLORS = {
+    divBlu:     [ '#3B76B3', '#FFFFFF', '#CC6171' ],
+    divBluAlt:  [ '#3B76B3', '#F3F3F3', '#CC6171' ],
+    divGrn:     [ '#4A807C', '#FFFFFF', '#A67D5A' ],
+    divGrnAlt:  [ '#4A807C', '#F3F3F3', '#A67D5A' ]
+  };
+
 
   /* Scales
   ----------------------------------------------- */
-  [3, 6, 9, 12].forEach(function(n) {
-    module.exports['qual' + n] = function() { return d3.scale.ordinal().range(qualColors.slice(0, n)); };
+
+  Object.keys(QUAL_COLORS).forEach(function(key) {
+    module.exports[key] = function() {
+      return d3.scale.ordinal().range(QUAL_COLORS[key].map(convertLab));
+    };
   });
 
-  module.exports.qualRedBlu = function() { return d3.scale.ordinal().range(convertLab(["#3b76b3", "#cc6171"])); };
-  module.exports.qualGrnBrn = function() { return d3.scale.ordinal().range(convertLab(["#497f7b", "#a57c59"])); };
+  Object.keys(SEQ_COLORS).forEach(function(key) {
+    module.exports[key] = function() {
+      return interpolatedColorScale().range(SEQ_COLORS[key].map(convertLab));
+    };
+  });
 
-  var seqColorsFull = {
-      Blu: convertLab(["#DDE9FE", "#b5cceb", "#8cb0d9", "#6493c6", "#3b76b3", "#396899", "#385b80", "#364d66", "#333e4c"]),
-      Red: convertLab(["#fdebeb", "#f2c9cd", "#e5a7af", "#d98490", "#cc6171", "#ac5663", "#8d4b56", "#6d4048", "#4d353a", "#4c3439"]),
-      Grn: convertLab(["#d2dfde", "#b0c7c6", "#8eb0ad", "#6c9894", "#4a807c", "#436f6d", "#3b5e5e", "#344d4e", "#2c3c3f"]),
-      Brn: convertLab(["#e9dfd6", "#d8c7b7", "#c8ae98", "#b79579", "#a67d5a", "#906b51", "#795a48", "#62493e", "#4c3735"]),
-      BluGryRed: convertLab(["#3b76b3", "#6995c3", "#97b4d3", "#c5d4e3", "#f3f3f3", "#e9ced3", "#e0aab2", "#d68592", "#cc6171"]),
-      GrnGryBrn: convertLab(["#4a807c", "#749d9a", "#9fbab8", "#c9d6d5", "#f3f3f3", "#e0d5cd", "#cdb8a7", "#b99b80", "#a67d5a"])
+  Object.keys(DIV_COLORS).forEach(function(key) {
+    module.exports[key] = function() {
+      return interpolatedColorScale().range(DIV_COLORS[key].map(convertLab));
+    };
+  });
+
+
+  /* Color utilities
+  ----------------------------------------------- */
+  module.exports.darker = function(inColor) {
+    return convertLab(inColor).darker(DARKEN);
   };
 
-  Object.keys(seqColorsFull).forEach(function(c) {
-    module.exports['qual' + c] = function() { return d3.scale.ordinal().range(seqColorsFull[c]); };
-  });
 
-  var seqColorsPartial = {
-    Blu: ["#dce8fd", "#3a75b2", "#333e4c"],
-    Red: ["#fdebeb", "#cb6070", "#4c3439"],
-    Grn: ["#d1dedd", "#497f7b", "#2b3b3e"],
-    Brn: ["#e8ded5", "#a57c59", "#4b3634"],
-    BluWhtRed: ["#3a75b2", "#ffffff", "#cb6070"],
-    BluGryRed: ["#3a75b2", "#f2f2f2", "#cb6070"],
-    GrnWhtBrn: ["#497f7b", "#ffffff", "#a57c59"],
-    GrnGryBrn: ["#497f7b", "#f2f2f2", "#a57c59"]
+  /* Helper functions
+  ----------------------------------------------- */
+  var convertLab = sszvis.fn.arity(1, d3.lab);
+
+  var interpolatedColorScale = function() {
+    var alteredScale = d3.scale.linear();
+    var nativeDomain = alteredScale.domain;
+
+    alteredScale.domain = function(dom) {
+      if (arguments.length === 1) {
+        var threeDomain = [dom[0], d3.mean(dom), dom[1]];
+        return nativeDomain.call(this, threeDomain);
+      } else {
+        return nativeDomain.apply(this, arguments);
+      }
+    };
+
+    return alteredScale;
   };
-
-  ['linear', 'log', 'pow'].forEach(function(s) {
-    Object.keys(seqColorsPartial).forEach(function(c) {
-      module.exports[s + c] = function() { return d3.scale.[s]().range(seqColorsPartial[c]); };
-    });
-  });
 
 });
