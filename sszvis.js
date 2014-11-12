f80dcc7f262a1eb7f734366419d586213c1d6fa3
Peter Gassner <peter@naehrstoff.ch>
2014-11-12 12:19:46 +0100

    Use ratio constant for bounds calculation

--- sszvis.js
+++ sszvis.js
@@ -1190,11 +1190,17 @@ namespace('sszvis.axis', function(module) {
  * @module sszvis/bounds
  * @see http://bl.ocks.org/mbostock/3019563
  *
+ * @property {number} DEFAULT_WIDTH The default width used across all charts
+ * @property {number} RATIO The default side length ratio
+ *
  * @param  {Object} bounds
  * @return {Object}
  */
 namespace('sszvis.bounds', function(module) {
   'use strict';
+
+  var DEFAULT_WIDTH = 516;
+  var RATIO = Math.sqrt(2);
 
   module.exports = function(bounds) {
     bounds || (bounds = {});
@@ -1204,8 +1210,8 @@ namespace('sszvis.bounds', function(module) {
       bottom: sszvis.fn.either(bounds.bottom, 0),
       left:   sszvis.fn.either(bounds.left, 1)
     };
-    var height  = sszvis.fn.either(bounds.height, 365 + padding.top + padding.bottom);
-    var width   = sszvis.fn.either(bounds.width, 516);
+    var width   = sszvis.fn.either(bounds.width, DEFAULT_WIDTH);
+    var height  = sszvis.fn.either(bounds.height, Math.round(width / RATIO) + padding.top + padding.bottom);
 
     return {
       innerHeight: height - padding.top  - padding.bottom,
@@ -1216,7 +1222,8 @@ namespace('sszvis.bounds', function(module) {
     };
   };
 
-  module.exports.SSZVIS_DEFAULT_WIDTH = 516;
+  module.exports.DEFAULT_WIDTH = DEFAULT_WIDTH;
+  module.exports.RATIO = RATIO;
 
 });
 
