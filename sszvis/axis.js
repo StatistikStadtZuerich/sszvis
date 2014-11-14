2436cb24b003cd3853d6a7fa4bf96bc1e0135da1
Peter Gassner <peter@naehrstoff.ch>
2014-11-14 11:13:14 +0100

    Axis: defer overlap calculation to the end
    Axis labels first need to be positioned correctly before we can calculate whether they overlap.

--- sszvis/axis.js
+++ sszvis/axis.js
@@ -100,35 +100,6 @@ namespace('sszvis.axis', function(module) {
             });
 
 
-          // Highlight axis labels that return true for props.highlightTick.
-          // Hide axis labels that overlap with highlighted labels.
-          if (props.highlightTick) {
-            var activeBounds = [];
-            var passiveBounds = [];
-            group.selectAll('.tick text')
-              .classed('hidden', false)
-              .classed('active', props.highlightTick)
-              .each(function(d) {
-                var bounds = {
-                  node: this,
-                  bounds: this.getBoundingClientRect()
-                };
-                if (props.highlightTick(d)) {
-                  bounds.left  -= LABEL_PROXIMITY_THRESHOLD;
-                  bounds.right += LABEL_PROXIMITY_THRESHOLD;
-                  activeBounds.push(bounds);
-                } else {
-                  passiveBounds.push(bounds);
-                }
-              });
-
-            activeBounds.forEach(function(active) {
-              passiveBounds.forEach(function(passive) {
-                d3.select(passive.node).classed('hidden', boundsOverlap(passive.bounds, active.bounds));
-              });
-            });
-          }
-
           if (props.tickColor) {
             group.selectAll('.tick line')
               .style('stroke', props.tickColor);
@@ -181,6 +152,35 @@ namespace('sszvis.axis', function(module) {
           if (props.slant) {
             group.selectAll('text')
               .call(slantLabel[axisDelegate.orient()][props.slant]);
+          }
+
+          // Highlight axis labels that return true for props.highlightTick.
+          // Hide axis labels that overlap with highlighted labels.
+          if (props.highlightTick) {
+            var activeBounds = [];
+            var passiveBounds = [];
+            group.selectAll('.tick text')
+              .classed('hidden', false)
+              .classed('active', props.highlightTick)
+              .each(function(d) {
+                var bounds = {
+                  node: this,
+                  bounds: this.getBoundingClientRect()
+                };
+                if (props.highlightTick(d)) {
+                  bounds.left  -= LABEL_PROXIMITY_THRESHOLD;
+                  bounds.right += LABEL_PROXIMITY_THRESHOLD;
+                  activeBounds.push(bounds);
+                } else {
+                  passiveBounds.push(bounds);
+                }
+              });
+
+            activeBounds.forEach(function(active) {
+              passiveBounds.forEach(function(passive) {
+                d3.select(passive.node).classed('hidden', boundsOverlap(passive.bounds, active.bounds));
+              });
+            });
           }
 
           if (props.title) {
