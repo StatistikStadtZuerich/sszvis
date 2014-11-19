fb57a498d7a4f7e4a808c23f5eb15e67f4b42df8
Mark Hintz <mark@interactivethings.com>
2014-11-19 15:32:18 +0100

    Document sszvis.bounds

--- sszvis/bounds.js
+++ sszvis/bounds.js
@@ -1,6 +1,7 @@
 /**
  * Creates a bounds object to help with the construction of d3 charts
- * that follow the d3 margin convention.
+ * that follow the d3 margin convention. The result of this function
+ * is comsumed by sszvis.createSvgLayer and sszvis.createHtmlLayer.
  *
  * @module sszvis/bounds
  * @see http://bl.ocks.org/mbostock/3019563
@@ -8,8 +9,17 @@
  * @property {number} DEFAULT_WIDTH The default width used across all charts
  * @property {number} RATIO The default side length ratio
  *
- * @param  {Object} bounds
- * @return {Object}
+ * @param  {Object} bounds       Specifies the bounds of a chart area. Valid properties are:
+ *                               width: the total width of the chart (default: DEFAULT_WIDTH)
+ *                               height: the total height of the chart (default: height / RATIO)
+ *                               top: top padding (default: 0)
+ *                               left: left padding (default: 1)
+ *                               bottom: bottom padding (default: 0)
+ *                               right: right padding (default: 1)
+ * @return {Object}              The returned object will preserve the properties width and height, or give them default values
+ *                               if unspecified. It will also contain 'innerWidth', which is the width minus left and right padding,
+ *                               and 'innerHeight', which is the height minus top and bottom padding. And it includes a 'padding' sub-object,
+ *                               which contains calculated or default values for top, bottom, left, and right padding.
  */
 namespace('sszvis.bounds', function(module) {
   'use strict';
