edde8cdbb48b2d9409be2128855bc65d9fd3e482
Peter Gassner <peter@naehrstoff.ch>
2014-11-19 09:29:41 +0100

    Refactor handleRuler to be in line with ruler
    This can be refactored much more, if a need arises

--- sszvis/component/handleRuler.js
+++ sszvis/component/handleRuler.js
@@ -9,72 +9,74 @@ namespace('sszvis.component.handleRuler', function(module) {
 
   module.exports = function() {
     return d3.component()
-      .prop('x')
-      .prop('y')
+      .prop('x', d3.functor)
+      .prop('y', d3.functor)
       .prop('top')
       .prop('bottom')
-      .prop('label').label('')
+      .prop('label').label(d3.functor(''))
       .prop('color')
       .prop('flip', d3.functor).flip(false)
       .render(function(data) {
         var selection = d3.select(this);
         var props = selection.props();
 
+        var crispX = sszvis.fn.compose(sszvis.fn.roundPixelCrisp, props.x);
         var crispY = sszvis.fn.compose(sszvis.fn.roundPixelCrisp, props.y);
 
-        var crispX = sszvis.fn.roundPixelCrisp(props.x);
         var bottom = props.bottom - 4;
         var handleWidth = 10;
         var handleHeight = 24;
         var handleTop = props.top - handleHeight;
 
-        var group = selection.selectAll('.sszvis-rule__group')
+        var group = selection.selectAll('.sszvis-handleRuler__group')
           .data([0]);
 
         var entering = group.enter()
           .append('g')
-          .classed('sszvis-rule__group', true);
+          .classed('sszvis-handleRuler__group', true);
 
         group.exit().remove();
 
         entering
           .append('line')
-          .classed('sszvis-rule__line', true);
+          .classed('sszvis-ruler__rule', true);
 
         entering
           .append('rect')
-          .classed('sszvis-rule__handle', true);
+          .classed('sszvis-handleRuler__handle', true);
 
         entering
           .append('line')
-          .classed('sszvis-rule__handle-mark', true);
+          .classed('sszvis-handleRuler__handle-mark', true);
 
-        group.selectAll('.sszvis-rule__line')
+        group.selectAll('.sszvis-ruler__rule')
           .attr('x1', crispX)
           .attr('y1', sszvis.fn.roundPixelCrisp(props.top))
           .attr('x2', crispX)
           .attr('y2', sszvis.fn.roundPixelCrisp(bottom));
 
-        group.selectAll('.sszvis-rule__handle')
-          .attr('x', crispX - handleWidth / 2)
+        group.selectAll('.sszvis-handleRuler__handle')
+          .attr('x', function(d) {
+            return crispX(d) - handleWidth / 2;
+          })
           .attr('y', sszvis.fn.roundPixelCrisp(handleTop))
           .attr('width', handleWidth)
           .attr('height', handleHeight)
           .attr('rx', 2)
           .attr('ry', 2);
 
-        group.selectAll('.sszvis-rule__handle-mark')
+        group.selectAll('.sszvis-handleRuler__handle-mark')
           .attr('x1', crispX)
           .attr('y1', sszvis.fn.roundPixelCrisp(handleTop + handleHeight * 0.15))
           .attr('x2', crispX)
           .attr('y2', sszvis.fn.roundPixelCrisp(handleTop + handleHeight * 0.85));
 
-        var dots = group.selectAll('.sszvis-rule__dot')
+        var dots = group.selectAll('.sszvis-ruler__dot')
           .data(data);
 
         dots.enter()
           .append('circle')
-          .classed('sszvis-rule__dot', true);
+          .classed('sszvis-ruler__dot', true);
 
         dots.exit().remove();
 
@@ -84,31 +86,45 @@ namespace('sszvis.component.handleRuler', function(module) {
           .attr('r', 3.5)
           .attr('fill', props.color);
 
-        var captions = group.selectAll('.sszvis-rule__label')
+
+        var labelOutline = selection.selectAll('.sszvis-ruler__label-outline')
           .data(data);
 
-        captions.enter()
+        labelOutline.enter()
           .append('text')
-          .classed('sszvis-rule__label', true);
+          .classed('sszvis-ruler__label-outline', true);
 
-        captions.exit().remove();
+        labelOutline.exit().remove();
 
-        captions
-          .attr('x', crispX)
-          .attr('y', crispY)
-          .attr('dx', function(d) {
-            return props.flip(d) ? -10 : 10;
-          })
-          .attr('dy', function(d) {
-            var baselineShift = 5;
-            if (crispY(d) < props.top + baselineShift)    return 2 * baselineShift;
-            if (crispY(d) > bottom - baselineShift) return 0;
-            return baselineShift;
+
+        var label = selection.selectAll('.sszvis-ruler__label')
+          .data(data);
+
+        label.enter()
+          .append('text')
+          .classed('sszvis-ruler__label', true);
+
+        label.exit().remove();
+
+
+        // Update both labelOutline and labelOutline selections
+
+        selection.selectAll('.sszvis-ruler__label, .sszvis-ruler__label-outline')
+          .attr('transform', function(d) {
+            var x = sszvis.fn.compose(sszvis.fn.roundPixelCrisp, props.x)(d);
+            var y = sszvis.fn.compose(sszvis.fn.roundPixelCrisp, props.y)(d);
+
+            var dx = props.flip(d) ? -10 : 10;
+            var dy = (y < props.top + dy) ? 2 * dy
+                   : (y > props.bottom - dy) ? 0
+                   : 5;
+
+            return sszvis.fn.translateString(x + dx, y + dy);
           })
           .style('text-anchor', function(d) {
             return props.flip(d) ? 'end' : 'start';
           })
-          .text(props.label);
+          .html(props.label);
 
       });
   };
