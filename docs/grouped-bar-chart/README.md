948ff422ce89d47f380fafdec5117bd3bd2b7613
Mark Hintz <mark@interactivethings.com>
2014-11-20 18:47:57 +0100

    Document grouped bar chart

--- docs/grouped-bar-chart/README.md
+++ docs/grouped-bar-chart/README.md
@@ -1,14 +1,54 @@
-# Bar Chart
+# Grouped Bar Chart
 
-## Options
+> Grouped bar charts are suited to making direct comparisons between multiple sets of similar things, while also comparing those sets using another differentiating dimension.
 
-### Datenformat
+## sszvis.component.groupedBars
 
-### Konfiguration
+### Data structure
 
-## Examples
+This chart requires a set of subsets, where each subset is a group of data objects to be displayed as a bar.
 
-### Grouped Bar Chart
+### Configuration
+
+
+#### `groupedBars.groupScale(scale)`
+
+This should be a scale function for determining the correct group offset of a member of a group. This function is passed the group member, and should return a value for the group offset which is the same for all members of the group. The within-group offset (which is different for each member) is then added to this group offset in order to position the bars individually within the group. So, for instance, if the groups are based on the "city" property, the groupScale should return the same value for all data objects with "city = Zurich".
+
+#### `groupedBars.groupSize(size)`
+
+This property tells groupedBars how many bars to expect for each group. It is used to assist in calculating the within-group layout and size of the bars. This number is treated as the same for all groups. Groups with less members than this number will have visible gaps. (Note that having less members in a group is not the same as having a member with a missing value, which will be discussed later)
+
+#### `groupedBars.groupWidth(width)`
+
+The width of the groups. This value is treated as the same for all groups. The width available to the groups is divided up among the bars. Often, this value will be the result of calling .rangeBand() on a d3.scale.ordinal scale.
+
+#### `groupedBars.groupSpace(space)`
+
+The percentage of space between each group. (default: 0.05). Usually the default is fine here.
+
+#### `groupedBars.y(yPosition)`
+
+The y-position of the bars in the group. This function is given a data value and should return a y-value. It should be similar to other functions you have already seen for positioning bars.
+
+#### `groupedBars.height(height)`
+
+The height of the bars in the group. This function is given a data value and should return a height value. It should be similar to other functions you have already seen for setting the height of bars.
+
+#### `groupedBars.fill([fill])`
+
+A functor which gives the color for each bar (often based on the bar's group). This can be a string or a function.
+
+#### `groupedBars.stroke([stroke])`
+
+The stroke color for each bar (default: none)
+
+#### `groupedBars.defined([isDefined])`
+
+A predicate function which can be used to determine whether a bar has a defined value. (default: true). Any bar for which this function returns false, meaning that it has an undefined (missing) value, will be displayed as a faint "x" in the grouped bar chart. This is in order to distinguish bars with missing values from bars with very small values, which would display as a very thin rectangle.
+
+
+### Chart
 
 ```project
 {
@@ -31,6 +71,8 @@
 }
 ```
 
+## With negative and missing values
+
 ```project
 {
     "name": "grouped-bar-chart_gb-two-small",
@@ -51,6 +93,8 @@
     }
 }
 ```
+
+## With many values, some negative
 
 ```project
 {
--- sszvis/component/groupedBars.js
+++ sszvis/component/groupedBars.js
@@ -1,5 +1,45 @@
 /**
- * Grouped Bars
+ * Grouped Bars Component
+ *
+ * The grouped bars component is used to create grouped vertical bar charts.
+ *
+ * The input to the grouped bar component should be an array of arrays, where each inner
+ * array contains the bars for a single group. Each of the inner arrays becomes a group, and
+ * each element in those inner arrays becomes a bar.
+ *
+ * In addition to the raw data, the user must provide other information necessary for calculating
+ * the layout of the groups of bars, namely the number of bars in each group (this component requires that
+ * all groups have the same number of bars), a scale for finding the x-offset of each group (usually an
+ * instance of d3.scale.ordinal), a width for groups, and y- and height- scales for the bars in the group.
+ * Note that the number of bars in each group and the group width determines how wide each bar will be, and
+ * this width is calculated internally to the groupedBars component.
+ *
+ * The groups are calculated and laid out entirely by the groupedBars component.
+ *
+ * @property {scale} groupScale         This should be a scale function for determining the correct group offset of a member of a group.
+ *                                      This function is passed the group member, and should return a value for the group offset which
+ *                                      is the same for all members of the group. The within-group offset (which is different for each member)
+ *                                      is then added to this group offset in order to position the bars individually within the group.
+ *                                      So, for instance, if the groups are based on the "city" property, the groupScale should return
+ *                                      the same value for all data objects with "city = Zurich".
+ * @property {number} groupSize         This property tells groupedBars how many bars to expect for each group. It is used to assist in
+ *                                      calculating the within-group layout and size of the bars. This number is treated as the same for all
+ *                                      groups. Groups with less members than this number will have visible gaps. (Note that having less members
+ *                                      in a group is not the same as having a member with a missing value, which will be discussed later)
+ * @property {number} groupWidth        The width of the groups. This value is treated as the same for all groups. The width available to the groups
+ *                                      is divided up among the bars. Often, this value will be the result of calling .rangeBand() on a d3.scale.ordinal scale.
+ * @property {number} groupSpace        The percentage of space between each group. (default: 0.05). Usually the default is fine here.
+ * @property {function} y               The y-position of the bars in the group. This function is given a data value and should return
+ *                                      a y-value. It should be similar to other functions you have already seen for positioning bars.
+ * @property {function} height          The height of the bars in the group. This function is given a data value and should return
+ *                                      a height value. It should be similar to other functions you have already seen for setting the height of bars.
+ * @property {string, function} fill    A functor which gives the color for each bar (often based on the bar's group). This can be a string or a function.
+ * @property {string, function} stroke  The stroke color for each bar (default: none)
+ * @property {function} defined         A predicate function which can be used to determine whether a bar has a defined value. (default: true).
+ *                                      Any bar for which this function returns false, meaning that it has an undefined (missing) value,
+ *                                      will be displayed as a faint "x" in the grouped bar chart. This is in order to distinguish bars with
+ *                                      missing values from bars with very small values, which would display as a very thin rectangle.
+ *
  * @return {d3.component}
  */
 namespace('sszvis.component.groupedBars', function(module) {
