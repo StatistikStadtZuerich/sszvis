7943bc4e9e6968f447f362cb96c84fd22b17e264
Mark Hintz <mark@interactivethings.com>
2015-11-11 19:55:58 +0100

    Add the overlap reducing algorithm (optional) to the ruler annotation

--- sszvis/annotation/ruler.js
+++ sszvis/annotation/ruler.js
@@ -17,6 +17,15 @@
  * @property {string, function} color     A string or function to specify the color of the ruler dots.
  * @property {function} flip              A boolean or function which returns a boolean that specifies
  *                                        whether the labels on the ruler dots should be flipped. (they default to the right side)
+ * @property {function} labelId           An id accessor function for the labels. This is used to match label data to svg elements,
+ *                                        and it is used by the reduceOverlap algorithm to match calculated bounds and positions with
+ *                                        labels. The default implementation uses the x and y positions of each label, but when labels
+ *                                        overlap, these positions are the same (and one will be removed!). It's generally a good idea
+ *                                        to provide your own function here, but you should especially use this when multiple labels
+ *                                        could overlap with each other. Usually this will be some kind of category accessor function.
+ * @property {boolean} reduceOverlap      Use an iterative relaxation algorithm to adjust the positions of the labels (when there is more
+ *                                        than one label) so that they don't overlap. This can be computationally expensive, when there are
+ *                                        many labels that need adjusting. This is turned off by default.
  *
  * @return {d3.component}
  */
@@ -33,16 +42,16 @@ sszvis_namespace('sszvis.annotation.ruler', function(module) {
       .prop('label').label(d3.functor(''))
       .prop('color')
       .prop('flip', d3.functor).flip(false)
+      .prop('labelId', d3.functor)
+      .prop('reduceOverlap').reduceOverlap(false)
       .render(function(data) {
         var selection = d3.select(this);
         var props = selection.props();
 
-        var key = function(d) {
-          return props.x(d) + '_' + props.y(d);
-        };
+        var labelId = props.labelId || function(d) { return props.x(d) + '_' + props.y(d) };
 
         var ruler = selection.selectAll('.sszvis-ruler__rule')
-          .data(data, key);
+          .data(data, labelId);
 
         ruler.enter()
           .append('line')
@@ -57,7 +66,7 @@ sszvis_namespace('sszvis.annotation.ruler', function(module) {
         ruler.exit().remove();
 
         var dot = selection.selectAll('.sszvis-ruler__dot')
-          .data(data, key);
+          .data(data, labelId);
 
         dot.enter()
           .append('circle')
@@ -73,7 +82,7 @@ sszvis_namespace('sszvis.annotation.ruler', function(module) {
 
 
         var labelOutline = selection.selectAll('.sszvis-ruler__label-outline')
-          .data(data, key);
+          .data(data, labelId);
 
         labelOutline.enter()
           .append('text')
@@ -83,7 +92,7 @@ sszvis_namespace('sszvis.annotation.ruler', function(module) {
 
 
         var label = selection.selectAll('.sszvis-ruler__label')
-          .data(data, key);
+          .data(data, labelId);
 
         label.enter()
           .append('text')
@@ -92,12 +101,15 @@ sszvis_namespace('sszvis.annotation.ruler', function(module) {
         label.exit().remove();
 
 
-        // Update both labelOutline and labelOutline selections
+        // Update both label and labelOutline selections
 
-        selection.selectAll('.sszvis-ruler__label, .sszvis-ruler__label-outline')
+        var crispX = sszvis.fn.compose(sszvis.svgUtils.crisp.halfPixel, props.x);
+        var crispY = sszvis.fn.compose(sszvis.svgUtils.crisp.halfPixel, props.y);
+
+        var textSelection = selection.selectAll('.sszvis-ruler__label, .sszvis-ruler__label-outline')
           .attr('transform', function(d) {
-            var x = sszvis.fn.compose(sszvis.svgUtils.crisp.halfPixel, props.x)(d);
-            var y = sszvis.fn.compose(sszvis.svgUtils.crisp.halfPixel, props.y)(d);
+            var x = crispX(d);
+            var y = crispY(d);
 
             var dx = props.flip(d) ? -10 : 10;
             var dy = (y < props.top + dy) ? 2 * dy
@@ -110,6 +122,60 @@ sszvis_namespace('sszvis.annotation.ruler', function(module) {
             return props.flip(d) ? 'end' : 'start';
           })
           .html(props.label);
+
+        if (props.reduceOverlap) {
+          var THRESHOLD = 2;
+          var ITERATIONS = 10;
+          var labelBounds = [];
+          // Optimization for the lookup later
+          var labelBoundsIndex = {};
+
+          // Reset vertical shift (set by previous renders)
+          textSelection.attr('y', '');
+
+          // Create bounds objects
+          label.each(function(d) {
+            var bounds = this.getBoundingClientRect();
+            var item = {
+              top: bounds.top,
+              bottom: bounds.bottom,
+              dy: 0
+            };
+            labelBounds.push(item);
+            labelBoundsIndex[labelId(d)] = item;
+          });
+
+          // Sort array in place by vertical position
+          // (only supports labels of same height)
+          labelBounds.sort(function(a, b) {
+            return d3.ascending(a.top, b.top);
+          });
+
+          // Calculate overlap and correct position 
+          while (--ITERATIONS) {
+            labelBounds.forEach(function(firstLabel, index) {
+              labelBounds.slice(index + 1).forEach(function(secondLabel) {
+                var overlap = firstLabel.bottom - secondLabel.top;
+                if (overlap >= THRESHOLD) {
+                  var offset = overlap / 2;
+                  firstLabel.bottom -= offset;
+                  firstLabel.top -= offset;
+                  firstLabel.dy -= offset;
+                  secondLabel.bottom += offset;
+                  secondLabel.top += offset;
+                  secondLabel.dy += offset;
+                }
+              });
+            });
+          }
+
+          // Shift vertically to remove overlap
+          textSelection.attr('y', function(d) {
+            var label = labelBoundsIndex[labelId(d)];
+            return label.dy;
+          });
+
+        }
 
       });
   };
