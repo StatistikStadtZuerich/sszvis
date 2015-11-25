ca2f2435b4fbcd41967e7d2986dcdd88dd8613ca
Mark Hintz <mark@interactivethings.com>
2015-11-25 16:25:02 +0100

    Add doc comment in map/projections

--- sszvis/map/projections.js
+++ sszvis/map/projections.js
@@ -1,3 +1,14 @@
+/**
+ * A collection of functions which return map projection functions.
+ *
+ * These functions abstract the use of sszvis.map.utils.swissMapProjection with the correct cache key.
+ * One exists for each of the map types. Note that these functions only work if the correct map module has been loaded.
+ *
+ * Used in the examples inside map-extended for cases where a projection function is required.
+ *
+ * @param {Number} width            The width of the map to be rendered
+ * @param {Number} height           The height of the map to be rendered
+ */
 sszvis_namespace('sszvis.map.projection', function(module) {
 
   module.exports.zurichStadtKreise = function(width, height) {
@@ -10,6 +21,10 @@ sszvis_namespace('sszvis.map.projection', function(module) {
 
   module.exports.zurichWahlKreise = function(width, height) {
     return sszvis.map.utils.swissMapProjection(width, height, sszvis.map.zurichWahlKreiseMapData.featureData(), sszvis.map.utils.constants.WAHL_KREISE_KEY);
+  };
+
+  module.exports.zurichStatistischeZonen = function(width, height) {
+    return sszvis.map.utils.swissMapProjection(width, height, sszvis.map.zurichStatistischeZonenMapData.featureData(), sszvis.map.utils.constants.STATISTISCHE_ZONEN_KEY);
   };
 
   module.exports.zurichAgglomeration2012 = function(width, height) {
