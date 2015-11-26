c7d3343440aeacc84ae122801eb7ae01992c95aa
Mark Hintz <mark@interactivethings.com>
2015-11-26 10:32:05 +0100

    Add source documentation for map raster renderer

--- sszvis/map/renderer/raster.js
+++ sszvis/map/renderer/raster.js
@@ -1,3 +1,24 @@
+/**
+ * raster renderer component
+ *
+ * @module  sszvis/map/renderer/raster
+ *
+ * Used for rendering a raster layer within a map (can also be used in other contexts, but the map usage
+ * is the most straightforward). Requires a width and a height for the raster layer, a function which
+ * returns raster positions, and one which returns fill colors.
+ *
+ * @property {Boolean} debug         Whether to activate debug mode, which shows a red square over the whole
+ *                                   canvas, for testing alignment with other map layers.
+ * @property {Number} width          The width of the canvas
+ * @property {Number} height         The height of the canvas
+ * @property {Function} position     A function which takes a datum and returns a position for the corresponding
+ *                                   raster square, returned as [x, y] pairs.
+ * @property {Number} cellSide       The length (in pixels) of one side of each raster cell
+ * @property {Function} fill         The fill function. Takes a datum and should return a fill color for the datum's pixel.
+ * @property {Number} opacity        The opacity of the canvas. Defaults to 1
+ *
+ * @return {d3.component}
+ */
 sszvis_namespace('sszvis.map.renderer.raster', function(module) {
   'use strict';
 
@@ -6,7 +27,6 @@ sszvis_namespace('sszvis.map.renderer.raster', function(module) {
       .prop('debug').debug(false)
       .prop('width')
       .prop('height')
-      .prop('fill')
       .prop('position')
       .prop('cellSide').cellSide(2)
       .prop('fill', d3.functor)
