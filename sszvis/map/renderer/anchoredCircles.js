bb56bbdd2451f3452599a4e3f8de7a0e25be3911
Mark Hintz <mark@interactivethings.com>
2015-11-26 17:50:31 +0100

    Anchored circles component documentation

--- sszvis/map/renderer/anchoredCircles.js
+++ sszvis/map/renderer/anchoredCircles.js
@@ -1,3 +1,19 @@
+/**
+ * @module sszvis/map/anchoredCircles
+ *
+ * Creates circles which are anchored to the positions of map elements. Used in the "bubble chart".
+ * You will usually want to pass this component, configured, as the .anchoredShape property of a base
+ * map component.
+ *
+ * @property {Object} mergedData                    Used internally by the base map component which renders this. Is a merged dataset used to render the shapes
+ * @property {Function} mapPath                     Used internally by the base map component which renders this. Is a path generation function which provides projections.
+ * @property {Number, Function} radius              The radius of the circles. Can be a function which accepts a datum and returns a radius value.
+ * @property {Color, Function} fill                 The fill color of the circles. Can be a function
+ * @property {Color, Function} strokeColor          The stroke color of the circles. Can be a function
+ * @property {Boolean} transition                   Whether or not to transition the sizes of the circles when data changes. Default true
+ * 
+ * @return {d3.component}
+ */
 sszvis_namespace('sszvis.map.anchoredCircles', function(module) {
 
   module.exports = function() {
