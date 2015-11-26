d3fbc1267580e18740cc500320285e7b61be65a8
Mark Hintz <mark@interactivethings.com>
2015-11-26 18:27:02 +0100

    Sankey layout source code documentation

--- sszvis/layout/sankey.js
+++ sszvis/layout/sankey.js
@@ -1,3 +1,9 @@
+/**
+ * @module sszvis/layout/sankey
+ *
+ * A module of helper functions for computing the data structure
+ * and layout required by the sankey component.
+ */
 sszvis_namespace('sszvis.layout.sankey', function(module) {
   'use strict';
 
@@ -6,6 +12,35 @@ sszvis_namespace('sszvis.layout.sankey', function(module) {
     return function() { return ++id; };
   })();
 
+  /**
+   * sszvis.layout.sankey.prepareData
+   *
+   * Returns a data preparation component for the sankey data.
+   *
+   * Throughout the code, the rectangles representing entities are referred to as 'nodes', while
+   * the chords connection them which represent flows among those entities are referred to as 'links'.
+   * 
+   * @property {Array} apply                    Applies the preparation to a dataset of links. Expects a list of links, where the (unique) id
+   *                                            of the source node can be accessed with the source function, and the (unique) id of the target
+   *                                            can be accessed with the target function. Note that no source can have the same id as a target and
+   *                                            vice versa. The nodes are defined implicitly by the fact that they have a link going to them or
+   *                                            from them.
+   * @property {Function} source                An accessor function for getting the source of a link
+   * @property {Function} target                An accessor function for getting the target of a link
+   * @property {Function} value                 An accessor function for getting the value of a link. Must be a number. The total value of a node
+   *                                            is the greater of the sum of the values of its sourced links and its targeting links.
+   * @property {} descendingSort                Toggles the use of a descending value sort for the nodes
+   * @property {} ascendingSort                 Toggles the use of an ascending value sort for the nodes
+   * @property {Array(Array)} idLists           An array of arrays of id values. For each array of ids, the sankey diagram will create a column
+   *                                            of nodes. Each node should have links going to it or coming from it. All ids should be unique.
+   *
+   * @return {Function}                         The data preparation function. Can be called directly, or applied using the '.apply' function.
+   *         When called, returns an object with data to be used in constructing the chart.
+   *               @property {Array} nodes             An array of node data. Each one will become a rectangle in the sankey
+   *               @property {Array} links             An array of link data. Each one will become a path in the sankey
+   *               @property {Array} columnTotals      An array of column totals. Needed by the computeLayout function (and internally by the sankey component)
+   *               @property {Array} columnLengths     An array of column lengths (number of nodes). Needed by the computeLayout function.
+   */
   module.exports.prepareData = function() {
     var mGetSource = sszvis.fn.identity;
     var mGetTarget = sszvis.fn.identity;
@@ -171,7 +206,29 @@ sszvis_namespace('sszvis.layout.sankey', function(module) {
     return main;
   };
 
-  module.exports.computeLayout = function(columnLengths, columnTotals, columnPixels, spreadPixels) {
+  /**
+   * sszvis.layout.sankey.computeLayout
+   *
+   * Automatically computes visual display properties needed by the sankey component,
+   * including padding between each node, paddings for the tops of columns to vertically center
+   * them, the domain and range of values in the nodes (used for scaling the node rectangles),
+   * the node thickness, and the domain and range of the column positioning scale.
+   * 
+   * @param  {Array} columnLengths      An array of lengths (number of nodes) of each column in the diagram.
+   *                                    Used to compute optimal padding between nodes. Provided by the layout.sankey.prepareData function
+   * @param  {Array} columnTotals       An array of column totals (total of all values of all ndoes). Provided by the 
+   * @param  {Number} columnHeight      The vertical height available for the columns. The tallest column will be this height. (Usually bounds.innerHeight)
+   * @param  {Number} columnWidth       The width of all columns. The sankey chart will be this width. (Usually bounds.innerWidth)
+   * @return {Object}                   An object of configuration parameters to be passed to the sankey component
+   *         @property {Number} nodePadding         The amount of padding to add between nodes. pass to component.sankey.nodePadding
+   *         @property {Array} columnPaddings       An array of padding values for each column. Index into this with the columnIndex and return to component.sankey.columnPadding
+   *         @property {Array} valueDomain          The domain for the node size scale. Use to configure a linear scale for component.sankey.sizeScale
+   *         @property {Array} valueRange           The range for the node size scale. Use to configure a linear scale for component.sankey.sizeScale
+   *         @property {Number} nodeThickness       The thickness of nodes. Pass to component.sankey.nodeThickness
+   *         @property {Array} columnDomain         The domain for the coumn position scale. use to configure a linear scale for component.sankey.columnPosition
+   *         @property {Array} columnRange          The range for the coumn position scale. use to configure a linear scale for component.sankey.columnPosition
+   */
+  module.exports.computeLayout = function(columnLengths, columnTotals, columnHeight, columnWidth) {
     // Calculate appropriate scale and padding values (in pixels)
     var padSpaceRatio = 0.15;
     var padMin = 12;
@@ -182,7 +239,7 @@ sszvis_namespace('sszvis.layout.sankey', function(module) {
     var computedPixPadding = d3.min(
       columnLengths.map(function(colLength) {
         // Any given column's padding is := (1 / 4 of total extent) / (number of padding spaces)
-        var colPadding = (columnPixels * padSpaceRatio) / (colLength - 1);
+        var colPadding = (columnHeight * padSpaceRatio) / (colLength - 1);
         // Limit by minimum and maximum pixel padding values
         return Math.max(padMin, Math.min(padMax, colPadding));
       })
@@ -194,7 +251,7 @@ sszvis_namespace('sszvis.layout.sankey', function(module) {
     var pixPerUnit = d3.min(
       columnLengths.map(function(colLength, colIndex) {
         // The non-padding pixels must have at least minDisplayPixels
-        var nonPaddingPixels = Math.max(minDisplayPixels, columnPixels - ((colLength - 1) * computedPixPadding));
+        var nonPaddingPixels = Math.max(minDisplayPixels, columnHeight - ((colLength - 1) * computedPixPadding));
         return nonPaddingPixels / columnTotals[colIndex];
       })
     );
@@ -220,7 +277,7 @@ sszvis_namespace('sszvis.layout.sankey', function(module) {
     // Calculate column (or row, as the case may be) positioning values
     var nodeThickness = 20;
     var numColumns = columnLengths.length;
-    var columnXMultiplier = (spreadPixels - nodeThickness) / (numColumns - 1);
+    var columnXMultiplier = (columnWidth - nodeThickness) / (numColumns - 1);
     var columnDomain = [0, 1];
     var columnRange = [0, columnXMultiplier];
 
