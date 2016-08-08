175841c129a66723d73fa99311a542e99c1bb39f
Mark Hintz <markohintz@gmail.com>
2016-08-08 16:54:39 +0200

    sszvis.behavior.resize documentation

--- sszvis/behavior/resize.js
+++ sszvis/behavior/resize.js
@@ -1,3 +1,32 @@
+/**
+ * Resize behavior
+ *
+ * The resize behavior can be used for alerting user code to changes in the browser window size.
+ * This includes window resizing behavior on desktop computers and browsers, but also orientation
+ * changes on mobile browsers. Functions which listen to the 'resize' event of a resize behavior
+ * component will be fired on window resize. You can add a resize listener to your application
+ * very easily:
+ *
+ * sszvis.behavior.resize().on('resize', listenerFunction);
+ *
+ * The listener function will be called once per resize event, but at a slight delay. This is because,
+ * while a user is resizing their browser window, many resize events can fire very quickly. This component
+ * automatically throttles the rate at which the listener function is called, since you probably don't need
+ * to respond to every single resize event. This throttling provides for a smoother user experience as they
+ * resize the browser, and increases performance across the board. The listener function will always be
+ * called after one or more window resize events, it just won't be called as often as the window fires the
+ * events.
+ *
+ * @module sszvis/behavior/resize
+ * 
+ * @property {string, function} on      the .on() method is used to listen to the resize event itself.
+ *                                      There is only one event supported by this behavior component, so
+ *                                      the first argument to .on must be 'resize' for it to have any effect.
+ *                                      Although this is somewhat redundant, it is done to keep this component's
+ *                                      API in line with the other behavior components.
+ * 
+ * @return {Object}
+ */
 sszvis_namespace('sszvis.behavior.resize', function(module) {
   'use strict';
 
