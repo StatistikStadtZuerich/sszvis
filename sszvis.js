368bbcebe3e10414f6de8dc8f57a09844e9a2940
Mark Hintz <markohintz@gmail.com>
2016-08-09 15:31:24 +0200

    Add annotation.tooltip.fit to the tooltip class
    Used for getting a sensible default tooltip orientation setter function which checks the position of the tooltip against the bounds of its container

--- sszvis.js
+++ sszvis.js
@@ -3909,6 +3909,19 @@ sszvis_namespace('sszvis.annotation.ruler', function(module) {
  * @property {function} opacity         A function or number which determines the opacity of the tooltip. Default is 1.
  *
  * @return {d3.component}
+ *
+ * @function sszvis.annotation.tooltip.fit
+ *
+ * This is a useful default function for making a tooltip fit within a horizontal space.
+ * You provide a default orientation for the tooltip, but also provide the bounds of the
+ * space within which the tooltip should stay. When the tooltip is too close to the left
+ * or right edge of the bounds, it is oriented away from the edge. Otherwise the default
+ * is used.
+ *
+ * @param {String} defaultValue         The default value for the tooltip orientation
+ * @param {Object} bounds               The bounds object within which the tooltip should stay.
+ *
+ * @returns {Function}                  A function for calculating the orientation of the tooltips.
  */
 sszvis_namespace('sszvis.annotation.tooltip', function(module) {
   'use strict';
@@ -3957,6 +3970,15 @@ sszvis_namespace('sszvis.annotation.tooltip', function(module) {
           .datum(tooltipData)
           .call(renderer);
       });
+  };
+
+  module.exports.fit = function(defaultVal, bounds) {
+    var lo = bounds.innerWidth * 1 / 4;
+    var hi = bounds.innerWidth * 3 / 4;
+    return function(d) {
+      var x = d.x;
+      return x > hi ? 'right' : x < lo ? 'left' : defaultVal;
+    };
   };
 
 
