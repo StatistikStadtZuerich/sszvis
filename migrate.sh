bd98709c9c3a0ec1027bb0ee9be3a5a7d2890076
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-05-26 11:03:20 +0200

    Remove migration scripts

--- migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eo pipefail
-
-git checkout HEAD -- sszvis/
-yarn run jscodeshift -- sszvis -t ./migrate.js
-yarn run rollup -- --config ./config/rollup/sszvis.js
-
-ls -lah dist/sszvis.js
