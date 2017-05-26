5671256a3465cbcdcca24ac06980d46ee033f175
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-05-26 10:22:31 +0200

    Add basic eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console": 0,
+        "no-extra-semi": 0,
+    },
+};
