9f0f1d5c112be70c38fd63c115361a16d885cc03
Jeremy Stucki <jeremy@interactivethings.com>
2017-09-13 12:23:29 +0200

    Add copyright banner to output

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,37 +1,45 @@
 import { join } from "path";
-import uglify from "rollup-plugin-uglify"
+import uglify from "rollup-plugin-uglify";
+import { version } from "../package.json";
 
-export default [{
-  input: join(__dirname, "..", "src", "index.js"),
-  output: [
-    {
-      file: join(__dirname, "..", "docs", "static", "sszvis.js"),
-      format: "umd",
-      name: "sszvis",
-    }
-  ],
-  external: ["d3", "topojson"],
-  globals: {
-    d3: "d3",
-    topojson: "topojson"
-  }
-},
-{
-  input: join(__dirname, "..", "src", "index.js"),
-  output: [
-    {
-      file: join(__dirname, "..", "docs", "static", "sszvis.min.js"),
-      format: "umd",
-      name: "sszvis",
-    }
-  ],
+const createConfig = ({ input, output, plugins }) => ({
+  input,
+  output,
+  plugins,
   external: ["d3", "topojson"],
   globals: {
     d3: "d3",
     topojson: "topojson"
   },
-  plugins: [
-    uglify()
-  ]
-}
+  banner: `/*! sszvis v${version}, Copyright 2014-present Statistik Stadt Zürich */`
+});
+
+export default [
+  createConfig({
+    input: join(__dirname, "..", "src", "index.js"),
+    output: [
+      {
+        file: join(__dirname, "..", "docs", "static", "sszvis.js"),
+        format: "umd",
+        name: "sszvis"
+      }
+    ]
+  }),
+  createConfig({
+    input: join(__dirname, "..", "src", "index.js"),
+    output: [
+      {
+        file: join(__dirname, "..", "docs", "static", "sszvis.min.js"),
+        format: "umd",
+        name: "sszvis"
+      }
+    ],
+    plugins: [
+      uglify({
+        output: {
+          comments: /^!/
+        }
+      })
+    ]
+  })
 ];
