d6251fb31bc65aca2247f0ff4635182f19446b8c
Jeremy Stucki <jeremy@interactivethings.com>
2017-12-14 14:21:02 +0100

    Don't show alert windows
    TODO: render an inline error in the chart instead

--- src/loadError.js
+++ src/loadError.js
@@ -8,13 +8,18 @@
 
 import * as logger from './logger.js';
 
-var RELOAD_MSG = 'Versuchen Sie, die Webseite neu zu laden. Sollte das Problem weiterhin bestehen, nehmen Sie mit uns Kontakt auf.';
+// var RELOAD_MSG = 'Versuchen Sie, die Webseite neu zu laden. Sollte das Problem weiterhin bestehen, nehmen Sie mit uns Kontakt auf.';
 
 export var loadError = function(error) {
   logger.error(error);
-  if (error.status === 404) {
-    alert('Die Daten konnten nicht geladen werden.\n\n' + error.responseURL + '\n\n' + RELOAD_MSG);
-  } else {
-    alert('Ein Fehler ist aufgetreten und die Visualisierung kann nicht angezeigt werden. ' + RELOAD_MSG);
-  }
+
+  // Don't use alert()!
+
+  // TODO: render an inline error in the chart instead
+
+  // if (error.status === 404) {
+  //   alert('Die Daten konnten nicht geladen werden.\n\n' + error.responseURL + '\n\n' + RELOAD_MSG);
+  // } else {
+  //   alert('Ein Fehler ist aufgetreten und die Visualisierung kann nicht angezeigt werden. ' + RELOAD_MSG);
+  // }
 };
