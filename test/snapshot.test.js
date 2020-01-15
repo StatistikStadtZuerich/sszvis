c6d4e7526c339aa54b43eb4f880166690ecccdc8
Peter Gassner <peter@gassner.io>
2020-01-15 16:02:08 +0100

    fix: Refine pixel-changed threshold for tests
    We set it to 10px difference to not catch tests that have changes below
    that threshold, which happened from time to time

--- test/snapshot.test.js
+++ test/snapshot.test.js
@@ -2,23 +2,29 @@
 
 import glob from "glob";
 
+const RENDER_DELAY = 200;
+const SNAPSHOT_OPTS = {
+  failureThreshold: 10,
+  failureThresholdType: "pixel"
+};
+
 const files = glob.sync("../docs/static/[^_]*/*.html", { cwd: __dirname });
 
 test.each(files.map(filepathToUrl))("%s", async url => {
   try {
     await page.goto(url, { waitUntil: "networkidle2" });
-    await page.waitFor(200);
+    await page.waitFor(RENDER_DELAY);
     const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot();
+    expect(image).toMatchImageSnapshot(SNAPSHOT_OPTS);
 
     const buttons = await page.$$(".sszvis-control-buttonGroup__item:not(.selected)");
 
     for (const button of buttons) {
       await button.click();
-      await page.waitFor(200);
+      await page.waitFor(RENDER_DELAY);
 
       const stateImage = await page.screenshot();
-      expect(stateImage).toMatchImageSnapshot();
+      expect(stateImage).toMatchImageSnapshot(SNAPSHOT_OPTS);
     }
   } catch (e) {
     expect(e).toBeNull();
