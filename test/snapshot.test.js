50c0658d7d929554d9f0b4fe8aa324b4758e4a0c
Peter Gassner <peter@gassner.io>
2020-01-15 14:31:03 +0100

    fix: Wait until chart rendered in snapshot test

--- test/snapshot.test.js
+++ test/snapshot.test.js
@@ -6,7 +6,8 @@ const files = glob.sync("../docs/static/[^_]*/*.html", { cwd: __dirname });
 
 test.each(files.map(filepathToUrl))("%s", async url => {
   try {
-    await page.goto(url);
+    await page.goto(url, { waitUntil: "networkidle2" });
+    await page.waitFor(100);
     const image = await page.screenshot();
     expect(image).toMatchImageSnapshot();
   } catch (e) {
