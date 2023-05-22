aee9d98301c1946267bd6671bfda46eca1b1a84f
João Sobral <joao.tiago.sobral@gmail.com>
2023-05-22 15:35:19 +0200

    feat: added stroked width to stacked area multiples

--- src/component/stackedAreaMultiples.js
+++ src/component/stackedAreaMultiples.js
@@ -25,41 +25,40 @@
  * @return {sszvis.component}
  */
 
+import { select, area } from "d3";
 
-import {select, area} from 'd3';
+import * as fn from "../fn.js";
+import { defaultTransition } from "../transition.js";
+import { component } from "../d3-component.js";
 
-import * as fn from '../fn.js';
-import { defaultTransition } from '../transition.js';
-import { component } from '../d3-component.js';
-
-export default function() {
+export default function () {
   return component()
-    .prop('x')
-    .prop('y0')
-    .prop('y1')
-    .prop('fill')
-    .prop('stroke')
-    .prop('key').key(function(d, i){ return i; })
-    .prop('valuesAccessor').valuesAccessor(fn.identity)
-    .prop('transition').transition(true)
-    .render(function(data) {
+    .prop("x")
+    .prop("y0")
+    .prop("y1")
+    .prop("fill")
+    .prop("stroke")
+    .prop("strokeWidth")
+    .prop("key")
+    .key(function (d, i) {
+      return i;
+    })
+    .prop("valuesAccessor")
+    .valuesAccessor(fn.identity)
+    .prop("transition")
+    .transition(true)
+    .render(function (data) {
       var selection = select(this);
       var props = selection.props();
 
       //sszsch why reverse?
       data = data.slice().reverse();
 
-      var areaGen = area()
-        .x(props.x)
-        .y0(props.y0)
-        .y1(props.y1);
+      var areaGen = area().x(props.x).y0(props.y0).y1(props.y1);
 
-      var paths = selection.selectAll('path.sszvis-path')
-        .data(data, props.key);
+      var paths = selection.selectAll("path.sszvis-path").data(data, props.key);
 
-      var newPaths = paths.enter()
-        .append('path')
-        .classed('sszvis-path', true);
+      var newPaths = paths.enter().append("path").classed("sszvis-path", true);
 
       paths.exit().remove();
 
@@ -70,8 +69,12 @@ export default function() {
       }
 
       paths
-        .attr('d', fn.compose(areaGen, props.valuesAccessor))
-        .attr('fill', props.fill)
-        .attr('stroke', props.stroke);
+        .attr("d", fn.compose(areaGen, props.valuesAccessor))
+        .attr("fill", props.fill)
+        .attr("stroke", props.stroke)
+        .attr(
+          "stroke-width",
+          props.strokeWidth !== undefined ? props.strokeWidth : 1
+        );
     });
-};
+}
