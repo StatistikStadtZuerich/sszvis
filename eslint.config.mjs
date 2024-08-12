191d6cad45f36c8d1d3aa39491c13fa53b8a74dc
lloydrichards <lloyd.d.richards@gmail.com>
2024-08-12 09:38:07 +0200

    chore: :technologist: add unicorn eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import unusedImports from "eslint-plugin-unused-imports";
 import sonarjs from "eslint-plugin-sonarjs";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
@@ -14,6 +15,22 @@ export default [
   },
   {
     ignores: ["contrib/", "build/", "docs/static/"],
+  },
+  eslintPluginUnicorn.configs["flat/recommended"],
+  {
+    rules: {
+      "unicorn/better-regex": "warn",
+      "unicorn/filename-case": "off",
+      "unicorn/prevent-abbreviations": "off",
+      "unicorn/no-lonely-if": "off",
+      "unicorn/prefer-module": "off",
+      "unicorn/numeric-separators-style": "warn",
+      "unicorn/prefer-number-properties": "warn",
+      "unicorn/no-array-callback-reference": "off",
+      "unicorn/no-array-reduce": "off",
+      "unicorn/no-null": "off",
+      
+    },
   },
   {
     rules: {
