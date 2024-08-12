1b7708b819d60c343ae226088d91afb8fd5c1b60
lloydrichards <lloyd.d.richards@gmail.com>
2024-08-12 09:24:06 +0200

    chore: :technologist: add unused-imports and sonar to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,31 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import unusedImports from "eslint-plugin-unused-imports";
+import sonarjs from "eslint-plugin-sonarjs";
 
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
+  sonarjs.configs.recommended,
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+  },
   {
     ignores: ["contrib/", "build/", "docs/static/"],
   },
   {
     rules: {
-      "no-unused-vars": [
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
         "warn",
         {
-          argsIgnorePattern: "^_",
+          vars: "all",
           varsIgnorePattern: "^_",
-          caughtErrorsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
         },
       ],
     },
