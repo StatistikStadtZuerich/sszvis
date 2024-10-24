70a5cb4898fc951e0bbe154645f567c84c33a65f
lloydrichards <lloyd.d.richards@gmail.com>
2024-10-24 15:47:19 +0200

    feat: :sparkles: add ruleLabelVerticalSeparate

--- src/annotation/index.js
+++ src/annotation/index.js
@@ -3,7 +3,7 @@ export { default as annotationLine } from "./line.js";
 export { default as annotationRangeFlag } from "./rangeFlag.js";
 export { default as annotationRangeRuler } from "./rangeRuler.js";
 export { default as annotationRectangle } from "./rectangle.js";
-export { default as annotationRuler } from "./ruler.js";
+export { annotationRuler, rulerLabelVerticalSeparate } from "./ruler.js";
 export { default as tooltip } from "./tooltip.js";
 export { default as tooltipAnchor } from "./tooltipAnchor.js";
 export { default as fitTooltip } from "./fitTooltip.js";
