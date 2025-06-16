58e2588a16aba5520ba8399ce9a92d434bbbff60
lloydrichards <lloyd.d.richards@gmail.com>
2025-06-16 16:11:41 +0200

    docs: remove params and return

--- src/fn.ts
+++ src/fn.ts
@@ -11,9 +11,6 @@ import { selection, Selection } from "d3";
  *
  * The identity function. It returns the first argument passed to it.
  * Useful as a default where a function is required.
- *
- * @param  {*} value any value
- * @return {*}       returns its argument
  */
 export const identity = function <T>(value: T): T {
   return value;
@@ -23,9 +20,6 @@ export const identity = function <T>(value: T): T {
  * fn.isString
  *
  * determine whether the value is a string
- *
- * @param  {*}  val       The value to check
- * @return {Boolean}      Whether the value is a string
  */
 export const isString = function (val: unknown): val is string {
   return Object.prototype.toString.call(val) === "[object String]";
@@ -35,9 +29,6 @@ export const isString = function (val: unknown): val is string {
  * fn.isSelection
  *
  * determine whether the value is a d3.selection.
- *
- * @param  {*}  val         The value to check
- * @return {Boolean}        Whether the value is a d3.selection
  */
 export const isSelection = function (val: unknown): val is Selection<any, any, any, any> {
   return val instanceof selection;
@@ -49,11 +40,6 @@ export const isSelection = function (val: unknown): val is Selection<any, any, a
  * Wraps a function of any arity (including nullary) in a function that
  * accepts exactly `n` parameters. Any extraneous parameters will not be
  * passed to the supplied function.
- *
- * @param {number} n The desired arity of the new function.
- * @param {Function} fn The function to wrap.
- * @return {Function} A new function wrapping `fn`. The new function is
- * guaranteed to be of arity `n`.
  */
 export const arity = function (n: number, fn: (...args: any[]) => any): (...args: any[]) => any {
   switch (n) {
@@ -163,9 +149,6 @@ export const arity = function (n: number, fn: (...args: any[]) => any): (...args
  * so that A(x) === F(G(H(x)))
  *
  * Note: all composed functions but the last should be of arity 1.
- *
- * @param {Function...} ... Accepts any number of functions as arguments
- * @return {Function} returns a function which is the composition of the passed functions
  */
 export const compose = function (...fns: ((...args: any[]) => any)[]): (...args: any[]) => any {
   const start = fns.length - 1;
@@ -181,10 +164,6 @@ export const compose = function (...fns: ((...args: any[]) => any)[]): (...args:
  * fn.contains
  *
  * Checks whether an item is present in the given list (by strict equality).
- *
- * @param  {array} list List of items
- * @param  {any}   d    Item that might be in list
- * @return {boolean}
  */
 export const contains = function <T>(list: T[], d: T): boolean {
   return list.includes(d);
@@ -194,9 +173,6 @@ export const contains = function <T>(list: T[], d: T): boolean {
  * fn.defined
  *
  * determines if the passed value is defined.
- *
- * @param  {*} val the value to check
- * @return {Boolean}     true if the value is defined, false if the value is undefined
  */
 export const defined = function <T>(val: T): val is NonNullable<T> {
   return val !== undefined && val != null && !Number.isNaN(val);
@@ -212,10 +188,6 @@ export const defined = function <T>(val: T): val is NonNullable<T> {
  * of those objects forms a set. This is distinct from other set functions in this toolkit because
  * in the other set functions, the set of derived properties is returned, whereas this function
  * returns a set of objects from the input array.
- *
- * @param  {array} arr        The array of elements from which the set is calculated
- * @param  {function} acc     An accessor function which calculates the set determiner.
- * @return {array}            An array of objects from the input array.
  */
 export const derivedSet = function <T>(
   arr: T[],
@@ -241,10 +213,6 @@ export const derivedSet = function <T>(
  *
  * Use a predicate function to test if every element in an array passes some test.
  * Returns false as soon as an element fails the predicate test. Returns true otherwise.
- *
- * @param  {Function} predicate     The predicate test function
- * @param  {Array} arr              The array to test
- * @return {Boolean}                Whether every element in the array passes the test
  */
 export const every = function <T>(predicate: (element: T) => boolean, arr: T[]): boolean {
   for (const element of arr) {
@@ -259,10 +227,6 @@ export const every = function <T>(predicate: (element: T) => boolean, arr: T[]):
  * fn.filledArray
  *
  * returns a new array with length `len` filled with `val`
- *
- * @param  {Number} len     The length of the desired array
- * @param  {Any} val        The value with which to fill the array
- * @return {Array}          An array of length len filled with val
  */
 export const filledArray = function <T>(len: number, val: T): T[] {
   const arr = Array.from({ length: len }) as T[];
@@ -276,11 +240,6 @@ export const filledArray = function <T>(len: number, val: T): T[] {
  * fn.find
  *
  * Finds the first occurrence of an element in an array that passes the predicate function
- *
- * @param {function} predicate A function that is run on each array element and returns a boolean
- * @param {array} arr An array
- *
- * @returns {arrayElement|undefined}
  */
 export const find = function <T>(predicate: (element: T) => boolean, arr: T[]): T | undefined {
   for (const element of arr) {
@@ -295,9 +254,6 @@ export const find = function <T>(predicate: (element: T) => boolean, arr: T[]): 
  * fn.first
  *
  * Returns the first value in the passed array, or undefined if the array is empty
- *
- * @param  {Array} arr an array
- * @return {*}     the first value in the array
  */
 export const first = function <T>(arr: T[]): T | undefined {
   return arr[0];
@@ -309,9 +265,6 @@ export const first = function <T>(arr: T[]): T | undefined {
  * Flattens the nested input array by one level. The input array is expected to be
  * a two-dimensional array (i.e. its elements are also arrays). The result is a
  * one-dimensional array consisting of all the elements of the sub-arrays.
- *
- * @param  {Array}        The Array to flatten
- * @return {Array}        A flattened Array
  */
 export const flatten = function <T>(arr: T[][]): T[] {
   return arr.flat();
@@ -344,9 +297,6 @@ export const firstTouch = function (event: TouchEvent): Touch | null {
  *
  * Used to lazily fold a sum type into a value.
  *
- * @param {string} key The key to use when folding
- * @param {object} pattern An object providing functions for each key
- *
  * @example
  * sszvis.foldPattern('formalGreeting', {
  *   formalGreeting: function() { return "Pleased to meet you."},
@@ -371,14 +321,6 @@ export const foldPattern = function <T>(key: string, pattern: Record<string, () 
  * in the input array (or the values returned by the accessor function)
  * MUST be "hashable" - convertible to unique keys of a JavaScript object.
  * As payoff for obeying this restriction, the algorithm can run much faster.
- *
- * @param  {Array} arr the Array of source elements
- * @param {Function} [acc(element, index, array)=(v) -> v] - an accessor function which
- * is called on each element of the Array. Defaults to the identity function.
- * The result is equivalent to calling array.map(acc) before computing the set.
- * When the accessor function is invoked, it is passed the element from the input array,
- * the element's index in the input array, and the input array itself.
- * @return {Array} an Array of unique elements
  */
 export const hashableSet = function <T, U extends string | number>(
   arr: T[],
@@ -402,9 +344,6 @@ export const hashableSet = function <T, U extends string | number>(
  * fn.isFunction
  *
  * Determines if the passed value is a function
- *
- * @param {*} val the value to check
- * @return {Boolean} true if the value is a function, false otherwise
  */
 export const isFunction = function (val: unknown): val is (...args: any[]) => any {
   return typeof val == "function";
@@ -414,9 +353,6 @@ export const isFunction = function (val: unknown): val is (...args: any[]) => an
  * fn.isNull
  *
  * determines if the passed value is null.
- *
- * @param {*} val the value to check
- * @return {Boolean}     true if the value is null, false if the value is not null
  */
 export const isNull = function (val: unknown): val is null {
   return val === null;
@@ -426,9 +362,6 @@ export const isNull = function (val: unknown): val is null {
  * fn.isNumber
  *
  * determine whether the value is a number
- *
- * @param  {*}  val     The value to check
- * @return {Boolean}    Whether the value is a number
  */
 export const isNumber = function (val: unknown): val is number {
   return Object.prototype.toString.call(val) === "[object Number]" && !Number.isNaN(val);
@@ -439,9 +372,6 @@ export const isNumber = function (val: unknown): val is number {
  *
  * determines if the passed value is of an "object" type, or if it is something else,
  * e.g. a raw number, string, null, undefined, NaN, something like that.
- *
- * @param  {*}  value      The value to test
- * @return {Boolean}       Whether the value is an object
  */
 export const isObject = function (val: unknown): val is object {
   return Object(val) === val;
@@ -451,9 +381,6 @@ export const isObject = function (val: unknown): val is object {
  * fn.last
  *
  * Returns the last value in the passed array, or undefined if the array is empty
- *
- * @param  {Array} arr an array
- * @return {*}     the last value in the array
  */
 export const last = function <T>(arr: T[]): T | undefined {
   return arr[arr.length - 1];
@@ -465,9 +392,6 @@ export const last = function <T>(arr: T[]): T | undefined {
  * Takes as argument a function f and returns a new function
  * which calls f on its arguments and returns the
  * boolean opposite of f's return value.
- *
- * @param  {Function} f the argument function
- * @return {Function}   a new function which returns the boolean opposite of the argument function
  */
 export const not = function <T extends any[]>(f: (...args: T) => any): (...args: T) => boolean {
   return function (this: any, ...args: T): boolean {
@@ -482,10 +406,6 @@ export const not = function <T extends any[]>(f: (...args: T) => any): (...args:
  * for the named property. When the accessor function is called on an object,
  * it returns that object's value for the named property. (or undefined, if the object
  * does not contain the property.)
- *
- * @param  {String} key the name of the property for which an accessor function is desired
- * @return {Function}     A property-accessor function
- *
  */
 export const prop = function <K extends string | number | symbol>(
   key: K
@@ -504,10 +424,6 @@ export const prop = function <K extends string | number | symbol>(
  * the object. If either is false, it returns a default value. The default value is the second
  * parameter to propOr, and it is optional. (When you don't provide a default value, the returned
  * function will work fine, and if the object or property are `undefined`, it returns `undefined`).
- *
- * @param  {String} key         the name of the property for which an accessor function is desired
- * @param  {any} defaultVal     the default value to return when either the object or the requested property are undefined
- * @return {Function}           A property-accessor function
  */
 export const propOr = function <K extends string | number | symbol, D>(
   key: K,
@@ -530,14 +446,6 @@ export const propOr = function <K extends string | number | symbol, D>(
  * [2,1,1,6,8,6,5,3] -> [2,1,6,8,5,3]
  * ["b", a", "b", "b"] -> ["b", "a"]
  * [{obj1}, {obj2}, {obj1}, {obj3}] -> [{obj1}, {obj2}, {obj3}]
- *
- * @param {Array} arr - the Array of source elements
- * @param {Function} [acc(element, index, array)=(v) -> v] - an accessor function which
- * is called on each element of the Array. Defaults to the identity function.
- * The result is equivalent to calling array.map(acc) before computing the set.
- * When the accessor function is invoked, it is passed the element from the input array,
- * the element's index in the input array, and the input array itself.
- * @return {Array} an Array of unique elements
  */
 export const set = function <T, U>(
   arr: T[],
@@ -555,10 +463,6 @@ export const set = function <T, U>(
  *
  * Test an array with a predicate and determine whether some element in the array passes the test.
  * Returns true as soon as an element passes the test. Returns false otherwise.
- *
- * @param  {Function} predicate     The predicate test function
- * @param  {Array} arr              The array to test
- * @return {Boolean}                Whether some element in the array passes the test
  */
 export const some = function <T>(predicate: (element: T) => boolean, arr: T[]): boolean {
   for (const element of arr) {
@@ -575,10 +479,6 @@ export const some = function <T>(predicate: (element: T) => boolean, arr: T[]): 
  * Determines whether two values are equal when converted to strings. Useful for comparing
  * date objects, because two different date objects are not considered equal, even if they
  * represent the same date.
- *
- * @param  {any} a        the first value
- * @param  {any} b        the second value
- * @return {boolean}      Whether the provided values are equal when converted to strings.
  */
 export const stringEqual = function (
   a: { toString(): string },
