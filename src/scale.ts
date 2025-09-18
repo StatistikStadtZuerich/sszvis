ac59523027276761b524cb9569f7b7ef0a40587e
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-09-18 13:43:16 +0200

    .

--- src/scale.ts
+++ src/scale.ts
@@ -14,9 +14,9 @@ interface Scale {
  *
  * Used to determine the extent of a scale's range. Mimics a function found in d3 source code.
  *
- * @param scale The scale to be measured
- * @return The extent of the scale's range. Useful for determining how far
- *         a scale stretches in its output dimension.
+ * @param  {array} scale    The scale to be measured
+ * @return {array}          The extent of the scale's range. Useful for determining how far
+ *                          a scale stretches in its output dimension.
  */
 export const range = function (scale: Scale): [number, number] {
   // borrowed from d3 source - svg.axis
@@ -29,8 +29,8 @@ export const range = function (scale: Scale): [number, number] {
  *
  * Used to determine the extent of an array. Mimics a function found in d3 source code.
  *
- * @param domain An array, sorted in either ascending or descending order
- * @return The extent of the array, with the smaller term first.
+ * @param  {array} domain     an array, sorted in either ascending or descending order
+ * @return {array}            the extent of the array, with the smaller term first.
  */
 function extent(domain: any[]): [number, number] {
   // borrowed from d3 source - svg.axis
