ab5ca3d06568a31e3056a2068f35d9cec0e88e20
Lloyd Richards <lloyd.d.richards@gmail.com>
2026-08-21 10:09:53 +0200

    refactor: port the component index to typescript

---
+++ src/component/index.ts
@@ -0,0 +1,15 @@
+export { default as bar } from "./bar";
+export { default as dot } from "./dot";
+export * from "./groupedBars";
+export { default as line } from "./line";
+export { nestedStackedBarsVertical } from "./nestedStackedBar";
+export { default as pack } from "./pack";
+export { default as pie } from "./pie";
+export { default as pyramid } from "./pyramid";
+export { default as sankey } from "./sankey";
+export { default as stackedArea } from "./stackedArea";
+export { default as stackedAreaMultiples } from "./stackedAreaMultiples";
+export * from "./stackedBar";
+export * from "./stackedPyramid";
+export { default as sunburst } from "./sunburst";
+export { default as treemap } from "./treemap";
