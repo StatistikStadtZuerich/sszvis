a02c17e96e78853bf26dadf447a865e5fdf82795
Lloyd Richards <lloyd.d.richards@gmail.com>
2026-09-06 23:57:25 +0200

    refactor: port the control index to typescript

---
+++ src/control/index.ts
@@ -0,0 +1,18 @@
+export {
+  type ButtonGroupChangeHandler,
+  type ButtonGroupComponent,
+  default as buttonGroup,
+} from "./buttonGroup.js";
+export { default as handleRuler, type HandleRulerComponent } from "./handleRuler.js";
+export {
+  default as selectMenu,
+  type SelectChangeHandler,
+  type SelectComponent,
+} from "./select.js";
+export {
+  default as slider,
+  type SliderChangeHandler,
+  type SliderComponent,
+  type SliderScale,
+  type SliderValue,
+} from "./slider.js";
